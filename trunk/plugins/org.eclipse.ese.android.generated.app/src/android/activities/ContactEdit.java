ad06b2028f90ff8ad81eaf338d5e6b8dc079bd13
mikael.barbero <mikael.barbero@3ab2182c-27ab-081f-8157-48715a0f5cb3>
2010-10-28 08:13:48 +0000

    Share project "org.eclipse.ese.android.generated.app" into "https://svn.codespot.com/a/eclipselabs.org/ese2010-generate-android-app-tutorial" Share project "org.eclipse.ese.android.app.models" into "https://svn.codespot.com/a/eclipselabs.org/ese2010-generate-android-app-tutorial" Share project "org.eclipse.ese.android.generator" into "https://svn.codespot.com/a/eclipselabs.org/ese2010-generate-android-app-tutorial"

---
+++ trunk/plugins/org.eclipse.ese.android.generated.app/src/android/activities/ContactEdit.java
@@ -0,0 +1,180 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2007, 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package android.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+/**
+ * ContactActivity.
+ */
+public class ContactEdit extends Activity {
+	
+	private static final int ACTIVITY_NEXT = 7;
+
+	private EditText wFirstName;
+
+	private EditText wLastName;
+
+	private EditText wPhoneNumber;
+
+	private EditText wEmail;
+
+	private Spinner wCountry;
+
+	private Button wSave;
+
+	private Button wNext;
+
+	
+	private Long rowId;
+    private MyContactDbAdapter dbHelper;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle instanceState) {
+		super.onCreate(instanceState);
+		dbHelper = new MyContactDbAdapter(this);
+	    dbHelper.open();
+		setContentView(R.layout.edit_contact);
+	
+		rowId = instanceState != null ? instanceState.getLong(MyContactDbAdapter.KEY_ROWID) : null;
+		if (rowId == null) {
+			Bundle extras = getIntent().getExtras();
+			rowId = extras != null ? extras.getLong(MyContactDbAdapter.KEY_ROWID) : null;
+		}
+		
+		initControls();
+		
+		populateFields();
+	}
+
+	private void initControls() {
+		wFirstName = (EditText) findViewById(R.id.wFirstName);
+
+		wLastName = (EditText) findViewById(R.id.wLastName);
+
+		wPhoneNumber = (EditText) findViewById(R.id.wPhoneNumber);
+
+		wEmail = (EditText) findViewById(R.id.wEmail);
+
+		wCountry = (Spinner) findViewById(R.id.wCountry);
+
+		wSave = (Button) findViewById(R.id.wSave);
+		wSave.setText("Save");
+		wSave.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				 setResult(RESULT_OK);
+	        	 finish();
+			}
+		});
+		
+		wNext = (Button) findViewById(R.id.wNext);
+		wNext.setText("Next");
+		wNext.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				if (rowId == null) {
+					saveOrCreate();
+				}
+			
+				Intent i = new Intent(v.getContext(), MoreContactInfoEdit.class);
+		        i.putExtra(MyContactDbAdapter.KEY_ROWID, rowId);
+		        startActivityForResult(i, ACTIVITY_NEXT);
+			}
+		});
+
+	}
+	
+    private void populateFields() {
+        if (rowId != null) {
+            Cursor c = dbHelper.fetchNote(rowId);
+            startManagingCursor(c);
+            
+            wFirstName.setText(c.getString(c.getColumnIndexOrThrow(MyContactDbAdapter.KEY_FIRSTNAME)));
+            
+            wLastName.setText(c.getString(c.getColumnIndexOrThrow(MyContactDbAdapter.KEY_LASTNAME)));
+            
+            wPhoneNumber.setText(c.getString(c.getColumnIndexOrThrow(MyContactDbAdapter.KEY_PHONENUMBER)));
+            
+            wEmail.setText(c.getString(c.getColumnIndexOrThrow(MyContactDbAdapter.KEY_EMAIL)));
+            
+            wCountry.setSelection(c.getInt(c.getColumnIndexOrThrow(MyContactDbAdapter.KEY_COUNTRY)));
+
+        }
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(MyContactDbAdapter.KEY_ROWID, rowId);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();  
+		saveOrCreate();
+    }
+    
+    private void saveOrCreate() {
+        String firstName = wFirstName.getText().toString();
+        
+        String lastName = wLastName.getText().toString();
+        
+        String phoneNumber = wPhoneNumber.getText().toString();
+        
+        String email = wEmail.getText().toString();
+        
+        int country = wCountry.getSelectedItemPosition();
+
+
+		if (rowId == null) {
+	        long id = dbHelper.createContact(firstName,lastName,phoneNumber,email,country);
+	        if (id > 0) {
+	            rowId = id;
+	        }
+	    } else {
+	        dbHelper.updateNote(rowId,firstName,lastName,phoneNumber,email,country);
+	    }
+    
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateFields();
+    }
+
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+
+		result.append(wFirstName.getText().toString());
+
+		result.append(wLastName.getText().toString());
+
+		result.append(wPhoneNumber.getText().toString());
+
+		result.append(wEmail.getText().toString());
+
+		result.append(wCountry.getSelectedItemPosition());
+
+		// We don't display the "Save" Button.
+
+
+		return result.toString();
+	}
+
+}
---
+++ trunk/plugins/org.eclipse.ese.android.generated.app/src/android/activities/MyContactDbAdapter.java
@@ -0,0 +1,203 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2007, 2008, 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package android.activities;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MyContactDbAdapter {
+
+    private static final String TAG = "MyContactDbAdapter";
+    
+    public static final String KEY_ROWID = "_id";
+
+    public static final String KEY_FIRSTNAME = "firstname";
+    
+    public static final String KEY_LASTNAME = "lastname";
+    
+    public static final String KEY_PHONENUMBER = "phonenumber";
+    
+    public static final String KEY_EMAIL = "email";
+    
+    public static final String KEY_COUNTRY = "country";
+
+    public static final String KEY_MORE = "more";
+    
+    public static final String KEY_MORE2 = "more2";
+    
+      
+    private static final String DATABASE_NAME = "mycontact_db";
+    private static final String DATABASE_TABLE = "mycontact";
+    private static final int DATABASE_VERSION = 2;
+
+    private static final String DATABASE_CREATE =
+            "create table mycontact (_id integer primary key autoincrement"
+    	
+                    + ", firstname text not null"
+
+                    + ", lastname text not null"
+
+                    + ", phonenumber text not null"
+
+                    + ", email text not null"
+
+                    + ", country integer not null"
+
+                    + ", more text not null"
+
+                    + ", more2 text not null"
+
+
+                    + ");";
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    private final Context context;
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion);
+            db.execSQL("DROP TABLE IF EXISTS mycontact");
+            onCreate(db);
+        }
+    }
+
+    public MyContactDbAdapter(Context context) {
+        this.context = context;
+    }
+
+    public MyContactDbAdapter open() throws SQLException {
+        dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+    
+    public void close() {
+        dbHelper.close();
+    }
+
+    public long createContact(String firstName, String lastName, String phoneNumber, String email, int country) {
+        ContentValues args = new ContentValues();
+        
+        args.put(KEY_FIRSTNAME, firstName);
+        
+        args.put(KEY_LASTNAME, lastName);
+        
+        args.put(KEY_PHONENUMBER, phoneNumber);
+        
+        args.put(KEY_EMAIL, email);
+        
+        args.put(KEY_COUNTRY, country);
+
+	
+        args.put(KEY_MORE2, "");
+        
+        args.put(KEY_MORE, "");
+        
+        
+        return db.insert(DATABASE_TABLE, null, args);
+    }
+    
+    public boolean updateNote(long rowId, String firstName, String lastName, String phoneNumber, String email, int country) {
+        ContentValues args = new ContentValues();
+        
+		args.put(KEY_FIRSTNAME, firstName);
+	        
+		args.put(KEY_LASTNAME, lastName);
+	        
+		args.put(KEY_PHONENUMBER, phoneNumber);
+	        
+		args.put(KEY_EMAIL, email);
+	        
+		args.put(KEY_COUNTRY, country);
+	
+        
+        return db.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+    public boolean updateNote(long rowId, String more, String more2) {
+        ContentValues args = new ContentValues();
+        
+		args.put(KEY_MORE, more);
+	        
+		args.put(KEY_MORE2, more2);
+	        
+        
+        return db.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    public boolean deleteMyContact(long rowId) {
+        return db.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+     public Cursor fetchAllMyContact() {
+        return db.query(DATABASE_TABLE, new String[] {KEY_ROWID
+        		
+        		, KEY_FIRSTNAME
+        		
+        		, KEY_LASTNAME
+        		
+        		, KEY_PHONENUMBER
+        		
+        		, KEY_EMAIL
+        		
+        		, KEY_COUNTRY
+
+        		, KEY_MORE
+        		
+        		, KEY_MORE2
+        		
+        		
+        	}, null, null, null, null, null);
+    }
+
+    public Cursor fetchNote(long rowId) throws SQLException {
+        Cursor mCursor = db.query(true, DATABASE_TABLE, new String[] {KEY_ROWID
+        		
+        		, KEY_FIRSTNAME
+        		
+        		, KEY_LASTNAME
+        		
+        		, KEY_PHONENUMBER
+        		
+        		, KEY_EMAIL
+        		
+        		, KEY_COUNTRY
+
+        		, KEY_MORE
+        		
+        		, KEY_MORE2
+        		
+        		
+        	}, KEY_ROWID + "=" + rowId, null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+}
